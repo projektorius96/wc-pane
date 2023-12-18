a58f02bcf94f9c32ea6241f2d20af5b4e3077a57
LG <34185919+projektorius96@users.noreply.github.com>
2023-12-18 16:09:46 +0200

    minor codebase revision

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 import { HUD, Input, Label } from "./src/index.js";
 
 const GUI = new HUD({container: document.body, minWidth: 15, position: 'right'})
-    GUI.addGroup({name: 'slider', nodes: GUI.addSection({accessor: 'section', column: 2})}) /* DEV_NOTE # accessor_valueN, N defaults to 1 */
+    ///* DEV_NOTE # accessor_valueN, N defaults to 1,2,3..n */
+    GUI.addGroup({name: 'slider', nodes: GUI.addSection({accessor: 'section', column: 2})})
     GUI.addGroup({name: 'describer', nodes: GUI.addSection({accessor: 'greet', column: 1})})
-const slider = GUI.find({name: 'slider'}).children;
 
-const PARAMS = {
+const rangeParams = {
     min: 1,
     max: 360,
     step: 1,
-    value: 1.
+    value: 1
 }
 
-const describer = GUI.find({name: 'describer'}).children;
 /* === describer */
+const describer = GUI.find({name: 'describer'}).children;
 describer.greet1.append(
         new Label('name it'),
         new Input({name: 'text_handle', type: 'text'})
 )
 GUI.find({name: 'text_handle'}).on('input', (e)=>console.log(e.target.value))
-
 /* === slider */
-const range = new Input({name: 'range', attrs: {...PARAMS}})
+const slider = GUI.find({name: 'slider'}).children;
+const range = new Input({name: 'range', attrs: {...rangeParams}})
 slider.section1.append(
     new Label('rotation'),
     range
@@ -29,9 +29,10 @@ slider.section1.append(
 GUI.find({name: range.name}).on('input', function(){
     console.log(this.value)
 });
-/* === checkbox */
+/* === checkboxer */
+const checkboxer = {name: 'tick1'}
 slider.section2.append(
     new Label('clock-wise'),
-    new Input({name: 'tick1', type: 'checkbox'/* , attrs: {cboxScaling: 1.5} */})
+    new Input({...checkboxer, type: 'checkbox'/* , attrs: {cboxScaling: 1.5} */})
 )
-GUI.find({name: 'tick1'}).on('change', (e)=>console.log(e.target.checked))
+GUI.find(checkboxer).on('change', (e)=>console.log(e.target.checked))
