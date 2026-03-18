511a84a5cea1c0df863ad9629b4be481df6a5325
LG <34185919+projektorius96@users.noreply.github.com>
2026-03-19 00:50:54 +0200

    code-rev: re-shaping GUI's api

--- implementation/main.js
+++ implementation/main.js
@@ -1,5 +1,5 @@
-import { Print } from "./utils.js";
 import { Pane, Input, Label } from "../src/index.js";
+import { Print } from "./utils.js";
 
 /**
  * @alias
@@ -11,9 +11,9 @@ const [ID, ATTR_TYPE, UI_EVENT] =
     ;
 
 const GUI = new Pane({container: document.body, draggable: true, hidden: false, position: Print.right, opacity: 1})
-    GUI.addGroup({name: ID.slider, open: true, nodes: GUI.addSection({sectionCount: 2, /* accessor: 'child' (DEFAULT) , */})})
-    GUI.addGroup({name: ID.describer, open: !true, nodes: GUI.addSection({})})
-    GUI.addGroup({name: ID.layer_manager, override_label: "layer-manager", open: !true, nodes: GUI.addSection({accessor: Print.slot})})
+    GUI.addGroup({name: ID.slider, nodes: GUI.addSection({flex_direction: Print.row})})
+    GUI.addGroup({name: ID.describer, nodes: GUI.addSection({})})
+    GUI.addGroup({name: ID.layer_manager, nodes: GUI.addSection({accessor: Print.slot}), override_label: "layer-manager", collapse: true})
 
 const rangeParams = {
     min: 1,
@@ -25,25 +25,20 @@ const rangeParams = {
 /* === GUI.describer === */
 const describer = GUI.find({name: ID.describer}).children;
 describer.child1.append(
-        /* new Label({description: "Sticky note"}), */
+        new Label({description: "Sticky note"}),
         new Input({name: ID.text_handle, type: input})
 )
 GUI.find({name: ID.text_handle}).on(UI_EVENT.input, (e)=>console.log(e.target.value))
 /* === GUI.slider === */
 const slider = GUI.find({name: ID.slider}).children;
 slider.child1.append(
-    /* new Label({description: "rotation"}), */
+    new Input({name: Print.tick1, type: checkbox}),
     new Input({name: ID.range, type: range, attrs: {...rangeParams}})
 );
+GUI.find({name: Print.tick1}).on(UI_EVENT.change, (e)=>console.log(e.target.checked))
 GUI.find({name: ID.range}).on(UI_EVENT.input, function() {
     console.log(this.value)
 });
-/* === GUI.checkboxer === */
-slider.child1.append(
-    new Label({description: "clock wise", textAlign: Print.center}),
-    new Input({name: Print.tick1, type: checkbox/* , attrs: {cboxScaling: 1.5} */})
-)
-GUI.find({name: Print.tick1}).on(UI_EVENT.change, (e)=>console.log(e.target.checked))
 /* === GUI.layerManager */
 const layerManager = GUI.find({name: ID.layer_manager}).children;
 layerManager.slot1.appendChild(
@@ -51,14 +46,14 @@ layerManager.slot1.appendChild(
     name: ID.layer_manager, 
     attrs: {
         loopData: [
-            function(item, index){ 
-                const ul = document.createElement(HTMLElement.extends?.(HTMLUListElement));
-                    ul.textContent = `No.${1+index} : ${item}`;
+            function(item, index) { 
+                const ul = document.createElement(Print.ul);
+                    ul.textContent = `${item}${1+index}`;
                 this.appendChild(ul);
             }
             , 
             [
-                ...Array.from({length: 3}).fill( Math.random() )
+                ...Array.from({length: 3}).fill( Print.HTMLElement )
             ]
         ], 
         sortableConfig: {
